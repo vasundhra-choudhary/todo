b54e180e0e742e0075f6fcfbfc16e23828f68430
Vasundhra Choudhary <vasu.choudhary9@gmail.com>
2024-12-30 17:11:10 +0530

    db changes

---
+++ todoList/db.py
@@ -0,0 +1,11 @@
+import sqlite3
+from flask import g, current_app
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+    return g.db
